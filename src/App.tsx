4d1c1f353c3a76b999e45c9112ad53a65ed76951
Naveen <notnaveen2@gmail.com>
2024-06-06 13:54:10 +0530

    added files

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './styles.css'; // Import your CSS file
+
+function Message() {
+  return (
+  <div className="container">
+    <img className='PitchathonIMG' src="src/assets/PLogo1.png" alt="" />
+    <div className="CS-text">COMING SOON</div>
+      <div className="lowerSection">
+        <div className="noti-text">Get notified when we launch</div>
+        <div className="subPortion">
+          <input className="email-bar" type="email" placeholder="Email" />
+          <button className="subButton">Subscribe</button>
+        </div>
+      </div>
+  </div>
+  );
+};
+export default Message;
