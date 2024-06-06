d66d521208627884f253fa3ac183fac7d2edd4f5
Naveen <notnaveen2@gmail.com>
2024-06-06 14:15:54 +0530

    resolved image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './styles.css'; // Import your CSS file
-
+import logo from './PLogo1.png'
 function Message() {
   return (
   <div className="container">
-    <img className='PitchathonIMG' src="src/assets/PLogo1.png" alt="" />
-    <img  src="1.png" alt="" />
+    <img className='PitchathonIMG' src= {logo} alt="" />
     <div className="CS-text">COMING SOON</div>
       <div className="lowerSection">
         <div className="noti-text">Get notified when we launch</div>
